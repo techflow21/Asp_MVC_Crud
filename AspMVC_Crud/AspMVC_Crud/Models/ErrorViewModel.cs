f91ad0033556ae68af224e689b587be2e4d5bb12
bello <bellosoliu12@gmail.com>
2022-11-10 21:47:45 +0100

    first commitment

---
+++ AspMVC_Crud/AspMVC_Crud/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspMVC_Crud.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
